21072d79ab0334dd4147d5bd9f4a5bd83900f55f
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-13 18:06:23 +0800

    Create main.js
    Hhh

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+require("content/turrets");
+
+Vars.content.units().each(e => e.health *= 2);
+Planets.sun.atmosphereRadOut = 10;
+Planets.sun.lightColor = Color.red;
+Planets.serpulo.orbitRadius = 5.6;
+if(!Vars.headless){
+  Core.app.post(() => {
+    const meta = Vars.mods.locateMod("lunaticmode").meta;
+    meta.displayName = "[purple]lunatic mode";
+    meta.author = "[#FCC21B]joshwelyt";
+    meta.description = "[red]why did you download this";
+  });
