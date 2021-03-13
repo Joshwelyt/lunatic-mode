7596d4d1dc92c9addf95b50919d6420edcd3af04
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-13 18:24:09 +0800

    Create blocks.js

---
+++ scripts/content/blocks.js
@@ -0,0 +1,8 @@
+Blocks.coreShard.unitCapModifier = 10;
+Blocks.coreFoundation.unitCapModifier = 20;
+Blocks.coreNucleus.unitCapModifier = 30;
+// all constructor time to 4000 :)
+Blocks.additiveReconstructor.constructTime = 4000;
+Blocks.multiplicativeReconstructor.constructTime = 4000;
+Blocks.exponentialReconstructor.constructTime = 5000;
+Blocks.tetrativeReconstructor.constructTime = 8000;
