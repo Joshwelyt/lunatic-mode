e6f3e9fb47b4c94556bf4227506a47b1468ba00c
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-16 07:00:10 +0800

    Update moontest.js
    Third moontest

--- scripts/moontest.js
+++ scripts/moontest.js
@@ -1,20 +1,7 @@
+//moon test third
 
-//moon test
-const moon = extend(Planet, "moon", serpulo, 0, 2.32, {});
-moon.atmosphereColor = Color.valueOf("a2a2a2");
-moon.atmosphereRadIn = 0.03;
+
+const moon = new JavaAdapter(Planet, {}, "moon", Planets.serpulo, 4, 0.7);
+moon.meshLoader = () => new SunMesh(moon, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("a2a2a2"), Color.valueOf("ffdd55"), Color.valueOf("a2a2a2"));
 moon.accessible = false;
-moon.meshLoader = () => new SunMesh(serpulo, 3,
-    5, 0.3, 1.7, 1.2, 1,
-    1.1,
-    Color.valueOf("a2a2a2"),
-    Color.valueOf("a2a2a2"),
-    Color.valueOf("a2a2a2"),
-    Color.valueOf("ffdd55"),
-    Color.valueOf("ffdd55"),
-    Color.valueOf("ffdd55")
-);
-
-module.exports = {
-    moon: moon
-};
+moon.hasAtmosphere = false;
