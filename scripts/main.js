d8ff0f2c754649f1ab53e285886c9f4ee699359d
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-16 12:38:21 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 require("content/turrets");
 require("content/blocks");
-require("campaign/planet");
+//require("campaign/planet");
 
 var hell = extend(SectorPreset, "hell", Planets.serpulo, 206, {
     localizedName: "Hell",
