4dc62ec91964653c2c9254e1cfae1c2a7f56ede5
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-18 18:56:37 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,3 @@ Planets.sun.lightColor = Color.purple;
 
 Planets.serpulo.orbitRadius = 5.6;
 Planets.serpulo.atmosphereColor = Color.purple;
-
-if(!Vars.headless){
-  Core.app.post(() => {
-    const meta = Vars.mods.locateMod("lunaticmode").meta;
-    meta.displayName = "[purple]lunatic mode";
-    meta.author = "[#FCC21B]joshwelyt";
-    meta.description = "[red]why did you download this";
-  });
