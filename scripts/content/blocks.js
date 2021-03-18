5757d6c8a168a948aa58edd93a44958e6a983cb3
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-18 18:30:32 +0800

    RESET INTIATE

--- scripts/content/blocks.js
+++
@@ -1,8 +0,0 @@
-Blocks.coreShard.unitCapModifier = 10;
-Blocks.coreFoundation.unitCapModifier = 20;
-Blocks.coreNucleus.unitCapModifier = 30;
-// all constructor time to 4000 :)
-Blocks.additiveReconstructor.constructTime = 4000;
-Blocks.multiplicativeReconstructor.constructTime = 4000;
-Blocks.exponentialReconstructor.constructTime = 5000;
-Blocks.tetrativeReconstructor.constructTime = 8000;
