ddd5bfc49eaeae4aa19ac48de21fdc3db0b9d261
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-18 21:05:59 +0800

    Update planets.js

--- scripts/content/planets.js
+++ scripts/content/planets.js
@@ -1,5 +1,5 @@
 //moon
 const moon = new JavaAdapter(Planet, {}, "moon", Planets.serpulo, 4, 0.7);
-moon.meshLoader = () => new SunMesh(moon, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("656565"), Color.valueOf("656565"), Color.valueOf("656565"));
+moon.meshLoader = () => new SunMesh(moon, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("515151"), Color.valueOf("656565"), Color.valueOf("515151"));
 moon.accessible = false;
 moon.hasAtmosphere = false;
