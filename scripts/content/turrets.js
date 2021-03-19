028b8be5912b2d4d0f904d47a71dbec796b235f3
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-20 07:05:48 +0800

    Update turrets.js

--- scripts/content/turrets.js
+++ scripts/content/turrets.js
@@ -1,15 +1,16 @@
 Vars.content.blocks().each(e => {
-    if(e instanceof Turret) {
-e.reloadTime = 100;
-e.spread = 30;
-e.inaccuracy = 80;
-e.recoilAmount = 2;
-e.restitution = 0;
-e.xRand = 0;
-e.cooldown = 150;
-e.rotateSpeed = 2;
-e.targetGround = true;
-e.targetAir = true;
+    if(e instanceof Turret && e.team == Team.sharded)
+ {
+   e.reloadTime = 100;
+   e.spread = 30;
+   e.inaccuracy = 80;
+   e.recoilAmount = 2;
+   e.restitution = 0;
+   e.xRand = 0;
+   e.cooldown = 150;
+   e.rotateSpeed = 2;
+   e.targetGround = true;
+   e.targetAir = true;
         if(typeof(e.shootType) !== "undefined") {
             if(typeof(e.shootType.collidesGround) !== "undefined") e.shootType.collidesGround = true;
             if(typeof(e.shootType.collidesAir) !== "undefined") e.shootType.collidesAir = true;
