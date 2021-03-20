6b0a9939066dc8d3649039ccadadf4ad2ba8d4a2
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-20 13:08:31 +0800

    Update turrets.js

--- scripts/content/turrets.js
+++ scripts/content/turrets.js
@@ -1,15 +1,16 @@
+//turret stuff
 Vars.content.blocks().each(e => {
     if(e instanceof Turret && e.team == Team.sharded) {
-            e.reloadTime = 100;
-            e.spread = 30;
-            e.inaccuracy = 80;
-            e.recoilAmount = 2;
-            e.restitution = 0;
-            e.xRand = 0;
-            e.cooldown = 150;
-            e.rotateSpeed = 2;
-            e.targetGround = true;
-            e.targetAir = true;
+        e.reloadTime = 80;
+        e.spread = 50;
+        e.inaccuracy = 80;
+        e.recoilAmount = 0;
+        e.restitution = 0;
+        e.xRand = 0;
+        e.cooldown = 100;
+        e.rotateSpeed = 2;
+        e.targetGround = true;
+        e.targetAir = true;
         if(typeof(e.shootType) !== "undefined") {
             if(typeof(e.shootType.collidesGround) !== "undefined") e.shootType.collidesGround = true;
             if(typeof(e.shootType.collidesAir) !== "undefined") e.shootType.collidesAir = true;
@@ -17,8 +18,11 @@ Vars.content.blocks().each(e => {
     }
 });
 
-Blocks.lancer.chargeTime = 140;
-Blocks.lancer.chargeMaxDelay = 140;
-Blocks.meltdown.range = 49;
-Blocks.meltdown.shootDuration = 50;
-Blocks.meltdown.shootType.length = 49;
+// Lancesuck
+Blocks.lancer.chargeTime = 50;
+Blocks.lancer.chargeMaxDelay = 50;
+
+// Meltshort
+Blocks.meltdown.range = 60;
+Blocks.meltdown.shootDuration = 60;
+Blocks.meltdown.shootType.length = 60;
